b96cd80fb7497e436f96b5607f78602e6782ec49
verdu <verdukactus@gmail.com>
2017-03-20 13:51:08 +0100

    ol list

--- data/tuto-info.js
+++ data/tuto-info.js
@@ -19,14 +19,14 @@ tutorialesApp.controller('TutorialesController', ['$scope', function ($scope) {
 
     var $videoToModal = $(event.currentTarget).children('.modal-video').attr('ng-src');
     var $tituloToModal = $(event.currentTarget).children('.modal-titulo').text();
-    var $comentariosToModal = $(event.currentTarget).children('.modal-comentarios').text();
     var $descripcionToModal = $(event.currentTarget).children('.modal-descripcion').text();
     var $materialesToModal = $(event.currentTarget).children('.modal-materiales').html();
     var $pasosToModal = $(event.currentTarget).children('.modal-pasos').html();
+    var $comentariosToModal = $(event.currentTarget).children('.modal-comentarios').html();
 
     $('.tut-video').attr('src', $videoToModal);
     $('.tut-titulo').text($tituloToModal);
-    $('.tut-comentarios').text($comentariosToModal);
+    $('.tut-comentarios').html($comentariosToModal);
     $('.tut-descripcion').text($descripcionToModal);
     $('.materiales-box').html($materialesToModal);
     $('.pasos-box').html($pasosToModal);
@@ -44,12 +44,33 @@ tutorialesApp.controller('TutorialesController', ['$scope', function ($scope) {
       "dificultad": "Fácil",
       "video": "https://www.youtube.com/embed/CsMmxYhIXg4",
       "coste": "10€",
-
       "descripcion": "Te traemos una idea original y totalmente personalizable, un álbum de fotos para pared que podrás colgar en casa y presumir de los recuerdos de tus últimas vacaciones.",
 
-      "materiales": ['Madera o listones', 'Cola blanca', 'Pintura a la tiza', 'Cartulina de color', 'Papel de scrapbook', 'Cinta de doble cara', 'Pegamento', 'Letras troqueladas', 'Adornos troquelados', 'Glossy accent'],
+      "materiales": [
+        'Madera o listones',
+        'Cola blanca',
+        'Pintura a la tiza',
+        'Cartulina de color',
+        'Papel de scrapbook',
+        'Cinta de doble cara',
+        'Pegamento',
+        'Letras troqueladas',
+        'Adornos troquelados',
+        'Glossy accent'
+      ],
 
-      "pasos": ['Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.', 'Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.', 'Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.'],
+      "pasos": [
+        'Une listones de madera con cola blanca hasta conseguir una tabla de 39’5×22’5cm y deja secar. Si lo prefieres puedes usar una pieza entera',
+        'Marca con lápiz una tira central que hará de separación. Pinta los bordes de la madera y el lomo central con pintura a la tiza del color que prefieras. Deja secar',
+        'Mide las dos zonas de madera sin pintar y crea 2 estructuras para álbum de fotos con cartulinas de diferentes tamaños. El lomo debe quedar en la parte superior',
+        'Corta las hojas de tu álbum a la medida que prefieras. Aquí hemos utilizado: 2uds de 16’3cm x 22’5cm; 2uds de 15x20cm; 2uds de 14×19’5cm y 2uds de 12’3×18’8cm',
+        'Elige papeles de scrap con diferentes diseños que combinen entre sí y corta trozos 1cm más pequeños de cada lado que las páginas de la estructura',
+        'Pega con cinta de doble cara a las cartulinas para decorar el interior y las tapas de tus álbumes de fotos dejando un borde de cartulina de 5mm alrededor que hará de marco',
+        'Pega la base de cada álbum con cinta de doble cara sobre la madera',
+        'Decora las tapas con papel de scrap y motivos troquelados. Con letras troqueladas puedes ponerle el nombre que prefieras a tus recuerdos',
+        'Si quieres colgarlo sólo tienes que atornillarle un cáncamo por detrás. Si te da pereza agujerear puedes ponerlo sobre una repisa',
+        'Ya sólo queda pegar tus fotos preferidas y el álbum cuadro estará listo para colgar'
+      ]
     },
 
     {
@@ -61,12 +82,29 @@ tutorialesApp.controller('TutorialesController', ['$scope', function ($scope) {
       "dificultad": "Fácil",
       "video": "https://www.youtube.com/embed/N-n-zdAbQk0",
       "coste": "5€",
-
       "descripcion": "Aprende a crear la estructura para un mini álbum de fotos desplegable que puedes decorar a tu gusto con la temática que quieras. Esta medida es perfecta para los recuerdos de una escapada.",
 
-      "materiales": ['4x cartulinas Din A4', 'Lápiz', 'Regla', 'Tijeras/guillotina', 'Plegadera', 'Cinta de doble cara'],
+      "materiales": [
+        '4x cartulinas Din A4',
+        'Lápiz', 'Regla',
+        'Tijeras/guillotina',
+        'Plegadera',
+        'Cinta de doble cara'
+      ],
 
-      "pasos": ['Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.', 'Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.', 'Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.'],
+      "pasos": [
+        'Colocando la primera cartulina en horizontal, marca arriba y abajo varios puntos a 11’5cm, 1’5cm y 11’5 cm del borde',
+        'Une los puntos dibujando líneas rectas con la ayuda de una regla y corta el trozo de cartulina sobrante',
+        'Dobla por las líneas y marca bien los pliegues con la ayuda de una plegadera o un plástico rígido',
+        'Marca arriba y abajo varios puntos en la segunda cartulina, a 11cm, 11cm y 1cm',
+        'Une los puntos de arriba y abajo con líneas rectas, luego dobla y marca bien los pliegues como en el caso anterior',
+        'Marca la tercera cartulina arriba y abajo a 11cm y 10cm. Corta el trozo de cartulina sobrante y dobla marcando bien los pliegues',
+        'Marca la cuarta cartulina a 11cm, 0’5cm y 11cm del borde. Corta el trozo sobrante y dobla marcando bien los pliegues',
+        'Une las cartulinas 2 y 3 con cinta de doble cara como se indica en el vídeo',
+        'Une la cartulina 4 al interior de estas dos con cinta de doble cara',
+        'Une este bloque de cartulinas a la cartulina 3 pegando el lomo a la zona central con cinta de doble cara como se indica en el vídeo',
+        'El resultado es un desplegable con páginas de diferente ancho para decorar con los motivos que más te gusten'
+      ]
     },
 
     {
@@ -83,7 +121,17 @@ tutorialesApp.controller('TutorialesController', ['$scope', function ($scope) {
 
       "materiales": ['Hojas tamaño din A4', 'Cúter', 'Regla metálica', 'Plegadera', 'Lápiz', 'Punzón', 'Hilo de algodón', 'Aguja de ojal grande'],
 
-      "pasos": ['Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.', 'Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.', 'Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.'],
+      "pasos": [
+        'Madera o listones',
+        'Cola blanca',
+        'Pintura a la tiza',
+        'Cartulina de color',
+        'Papel de scrapbook',
+        'Cinta de doble cara',
+        'Pegamento', 'Letras troqueladas',
+        'Adornos troquelados',
+        'Glossy accent'
+      ]
     },
 
     {
@@ -100,7 +148,17 @@ tutorialesApp.controller('TutorialesController', ['$scope', function ($scope) {
 
       "materiales": ['Hojas tamaño din A4', 'Cúter', 'Regla metálica', 'Plegadera', 'Lápiz', 'Punzón', 'Hilo de algodón', 'Aguja de ojal grande'],
 
-      "pasos": ['Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.', 'Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.', 'Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.'],
+      "pasos": [
+        'Madera o listones',
+        'Cola blanca',
+        'Pintura a la tiza',
+        'Cartulina de color',
+        'Papel de scrapbook',
+        'Cinta de doble cara',
+        'Pegamento', 'Letras troqueladas',
+        'Adornos troquelados',
+        'Glossy accent'
+      ]
     },
 
     {
@@ -116,6 +174,18 @@ tutorialesApp.controller('TutorialesController', ['$scope', function ($scope) {
       "descripcion": "Aprende a hacer divertidas diademas de flores reciclando cartones de hueveras. Esta manualidad es ideal para hacer con los peques de la casa una tarde de lluvia o como actividad en una fiesta de pijamas para princesas.",
 
       "materiales": ['Hojas tamaño din A4', 'Cúter', 'Regla metálica', 'Plegadera', 'Lápiz', 'Punzón', 'Hilo de algodón', 'Aguja de ojal grande'],
+
+      "pasos": [
+        'Madera o listones',
+        'Cola blanca',
+        'Pintura a la tiza',
+        'Cartulina de color',
+        'Papel de scrapbook',
+        'Cinta de doble cara',
+        'Pegamento', 'Letras troqueladas',
+        'Adornos troquelados',
+        'Glossy accent'
+      ]
     },
 
     {
@@ -132,7 +202,17 @@ tutorialesApp.controller('TutorialesController', ['$scope', function ($scope) {
 
       "materiales": ['Hojas tamaño din A4', 'Cúter', 'Regla metálica', 'Plegadera', 'Lápiz', 'Punzón', 'Hilo de algodón', 'Aguja de ojal grande'],
 
-      "pasos": ['Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.', 'Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.', 'Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.'],
+      "pasos": [
+        'Madera o listones',
+        'Cola blanca',
+        'Pintura a la tiza',
+        'Cartulina de color',
+        'Papel de scrapbook',
+        'Cinta de doble cara',
+        'Pegamento', 'Letras troqueladas',
+        'Adornos troquelados',
+        'Glossy accent'
+      ]
     },
 
     {
@@ -149,7 +229,17 @@ tutorialesApp.controller('TutorialesController', ['$scope', function ($scope) {
 
       "materiales": ['Hojas tamaño din A4', 'Cúter', 'Regla metálica', 'Plegadera', 'Lápiz', 'Punzón', 'Hilo de algodón', 'Aguja de ojal grande'],
 
-      "pasos": ['Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.', 'Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.', 'Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.'],
+      "pasos": [
+        'Madera o listones',
+        'Cola blanca',
+        'Pintura a la tiza',
+        'Cartulina de color',
+        'Papel de scrapbook',
+        'Cinta de doble cara',
+        'Pegamento', 'Letras troqueladas',
+        'Adornos troquelados',
+        'Glossy accent'
+      ]
     },
 
     {
@@ -166,7 +256,17 @@ tutorialesApp.controller('TutorialesController', ['$scope', function ($scope) {
 
       "materiales": ['Hojas tamaño din A4', 'Cúter', 'Regla metálica', 'Plegadera', 'Lápiz', 'Punzón', 'Hilo de algodón', 'Aguja de ojal grande'],
 
-      "pasos": ['Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.', 'Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.', 'Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.'],
+      "pasos": [
+        'Madera o listones',
+        'Cola blanca',
+        'Pintura a la tiza',
+        'Cartulina de color',
+        'Papel de scrapbook',
+        'Cinta de doble cara',
+        'Pegamento', 'Letras troqueladas',
+        'Adornos troquelados',
+        'Glossy accent'
+      ]
     },
 
     {
@@ -183,7 +283,17 @@ tutorialesApp.controller('TutorialesController', ['$scope', function ($scope) {
 
       "materiales": ['Hojas tamaño din A4', 'Cúter', 'Regla metálica', 'Plegadera', 'Lápiz', 'Punzón', 'Hilo de algodón', 'Aguja de ojal grande'],
 
-      "pasos": ['Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.', 'Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.', 'Incididunt cillum vidisse laboris, eu amet expetendis commodo, ut dolore praetermissum ita qui lorem domesticarum o illum excepteur comprehenderit, qui sint iudicem familiaritatem, dolor reprehenderit iudicem duis admodum non incididunt duis quae do quid. Quo offendit nam nescius an an culpa id amet, lorem laborum eu eiusmod, cillum proident ex distinguantur.'],
+      "pasos": [
+        'Madera o listones',
+        'Cola blanca',
+        'Pintura a la tiza',
+        'Cartulina de color',
+        'Papel de scrapbook',
+        'Cinta de doble cara',
+        'Pegamento', 'Letras troqueladas',
+        'Adornos troquelados',
+        'Glossy accent'
+      ]
     },
 
   ];
